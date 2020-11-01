2402b456e3b74b38a852caa579bc2f8b4c1ba92f
Curt Manning <cmann4ski@yahoo.com>
2020-10-31 20:09:47 -0600

    fix: js error

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    branches: "main"
-    repositoryUrl: "https://github.com/curtmanning/react-app"
+    branches: "main",
+    repositoryUrl: "https://github.com/curtmanning/react-app",
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
